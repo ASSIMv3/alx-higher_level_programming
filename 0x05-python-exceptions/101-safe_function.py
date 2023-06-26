d2f02ca67ef016f80983d9249bc4115355b632d8
ASSIMv3 <hassanassim01@gmail.com>
2023-06-26 21:33:37 +0100

    adv task 2

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,6 @@ def safe_function(fct, *args):
     import sys
     try:
         return fct(*args)
-    except:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
