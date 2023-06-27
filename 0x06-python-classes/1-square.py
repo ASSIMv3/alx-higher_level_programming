3fd64c358765f3acf4c0dc7eeafed97a67d16ad5
ASSIMv3 <hassanassim01@gmail.com>
2023-06-27 09:16:44 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size):
+        """Initializes a new instance of the Square class"""
+        self.__size = size
