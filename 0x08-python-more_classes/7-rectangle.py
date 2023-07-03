66634c4319fe89879b9b4b54e152c12df85cd077
ASSIMv3 <hassanassim01@gmail.com>
2023-07-03 09:16:36 -0700

    fixing task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -9,8 +9,6 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.__width = 0
-        self.__height = 0
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
@@ -23,9 +21,10 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -35,9 +34,10 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         return self.__width * self.__height
