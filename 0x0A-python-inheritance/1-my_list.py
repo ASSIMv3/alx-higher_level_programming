f9b92cf3bc6accd300c036f9c7ce033304258442
ASSIMv3 <hassanassim01@gmail.com>
2023-07-10 07:38:46 -0700

    mandatory task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,7 @@
 
 class MyList(list):
     def print_sorted(self):
+        """Sort the list in ascending order"""
         sorted_list = sorted(self)
+        """Print the sorted list"""
         print(sorted_list)
