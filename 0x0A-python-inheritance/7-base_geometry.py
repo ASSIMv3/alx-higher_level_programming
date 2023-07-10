6354b4670f5c496da78f646398c4acdca0d9140c
ASSIMv3 <hassanassim01@gmail.com>
2023-07-10 09:18:38 -0700

    mandatory task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,9 +6,11 @@ class BaseGeometry:
     """Base class representing geometry"""
     
     def area(self):
+        """Public instance method that raises an Exception with the message"""
         raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
+        """Public instance method that validates the value"""
         if type(value) != int and type(value) != float:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
