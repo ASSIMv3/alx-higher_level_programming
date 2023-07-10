3e89d2c6f0ccb27c8760653ef17b694ae98150ee
ASSIMv3 <hassanassim01@gmail.com>
2023-07-10 07:59:45 -0700

    mandatory task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if the object is an instance of the specified class"""
+    return isinstance(obj, a_class)
