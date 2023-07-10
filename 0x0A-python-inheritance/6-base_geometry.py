209696d6bf6a06f9093c1c0e66548ef3ed3476bd
ASSIMv3 <hassanassim01@gmail.com>
2023-07-10 08:09:36 -0700

    mandatory task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module base_geometry function"""
+
+
+class BaseGeometry():
+    """Base class representing geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
