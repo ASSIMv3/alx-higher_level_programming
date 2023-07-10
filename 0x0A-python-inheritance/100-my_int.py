243d13aa070119b6b3e07a5cf12eff1e35ff6670
ASSIMv3 <hassanassim01@gmail.com>
2023-07-10 09:49:18 -0700

    adv task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """MyInt class that inherits from int"""
     def __eq__(self, other):
         """Inverted equality operator"""
-        return not (self.real == other.real)
+        return int(self) != other
     def __ne__(self, other):
         """Inverted inequality operator"""
-        return not (self.real != other.real)
+        return int(self) == other
