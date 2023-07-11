5a5deb020117e9dbce2a1b7b6b095706b65eb9ed
ASSIMv3 <hassanassim01@gmail.com>
2023-07-11 10:13:21 -0700

    mandatory task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,11 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+
 try:
-    item  = load_from_json_file("add_item.json")
+    item = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    item  = []
+    item = []
 
 for i in range(1, len(sys.argv)):
     item.append(sys.argv[i])
