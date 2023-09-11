e8f8dcc0e7b66e2e3b0cb7bd21eaf335395234e7
ASSIMv3 <hassanassim01@gmail.com>
2023-09-11 10:29:31 -0700

    mandatory task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const n = parseInt(process.argv[2]);
+function factorial (number) {
+  if (isNaN(number) || number <= 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
+}
+console.log(factorial(n));
