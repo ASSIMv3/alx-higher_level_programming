f231757e4ea01f3f6b15feb111fa57afb33ff7bb
ASSIMv3 <hassanassim01@gmail.com>
2023-09-11 10:31:44 -0700

    mandatory task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const args = process.argv.slice(2).map(Number).sort().reverse();
+  const args = process.argv.slice(2);
+  args.sort(function (a, b) { return a - b; });
+  args.reverse();
   console.log(args[1]);
 }
