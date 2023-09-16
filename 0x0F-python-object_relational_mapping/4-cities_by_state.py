df207abfd1c1d5023376ec0550c15642076d16df
ASSIMv3 <hassanassim01@gmail.com>
2023-09-16 05:42:52 -0700

    mandatory task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """ lists all cities from the database hbtn_0e_4_usa"""
 
-import MySQLdb as db
+import MySQLdb
 from sys import argv
-if __name__ == '__main__':
-    db_connect = db.connect(host="localhost", port=3306, user=argv[1],
-                            passwd=argv[2], db=argv[3])
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT id, \
-                name, (SELECT name FROM states WHERE state_id=id), \
-                FROM cities ORDER BY id")
-        rows = db_cursor.fetchall()
-    if rows is not None:
-        for row in rows:
-            print(row)
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities JOIN states ON cities.state_id = states.id \
+                ORDER BY cities.id;")
+    cities = cur.fetchall()
+    for citie in cities:
+        print(citie)
+    cur.close()
+    db.close()
