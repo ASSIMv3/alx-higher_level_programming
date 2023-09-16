6738e1d6c3b31874de0cd62ce102d2c3ab216add
ASSIMv3 <hassanassim01@gmail.com>
2023-09-16 05:39:53 -0700

    mandatory task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """takes in arguments and displays all values in the states table"""
 
-import MySQLdb as db
+import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db_connect = db.connect(host="localhost", port=3306, user=argv[1],
-                            passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
-    db_cursor.execute("SELECT * FROM `states`")
-    rows = db_cursor.fetchall()
-    for row in rows:
-        print(row)
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    cur = db.cursor()
+    state_name = argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s;", (state_name, ))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
