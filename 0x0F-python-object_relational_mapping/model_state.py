d95a324188f317f1a37c188170ee4933f571fb29
ASSIMv3 <hassanassim01@gmail.com>
2023-09-16 05:51:33 -0700

    mandatory task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """ Class definition of a State and an instance Base"""
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
