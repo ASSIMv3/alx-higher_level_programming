12b4b3ce06480a0bca2bcd507d35347d63492638
ASSIMv3 <hassanassim01@gmail.com>
2023-09-28 09:26:10 -0700

    mandatory task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,9 @@
 
 
 def find_peak(list_of_integers):
-    if list_of_integers:
-        list_of_integers.sort(reverse=True)
-        return list_of_integers[0]
+    if type(list_of_integers) != list:
+        return
+    if len(list_of_integers) == 0:
+        return None
+    list_of_integers.sort(reverse=True)
+    return list_of_integers[0]
