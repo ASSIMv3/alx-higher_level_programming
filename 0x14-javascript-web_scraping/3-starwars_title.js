6bad068489722111d08df4ddf7e2273e1904a3ed
ASSIMv3 <hassanassim01@gmail.com>
2023-10-24 05:25:29 -0700

    mandatory task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+request(url, function (error, response, body) {
+  if (!error) {
+    const json_ = JSON.parse(body);
+    console.log(json_.title);
+  }
+});
