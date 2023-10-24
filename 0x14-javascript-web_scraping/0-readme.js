f32d1bd131a35d6a54d8674b061671083d0e2b0f
ASSIMv3 <hassanassim01@gmail.com>
2023-10-24 05:19:01 -0700

    mandatory task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(args, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(data);
+});
