a47e2b0be00ea4368a40c49728f25f8e819ccd17
ASSIMv3 <hassanassim01@gmail.com>
2023-11-01 06:15:27 -0700

    fix errors

--- 0x15-javascript-web_jquery/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const myArgs = process.argv.slice(2);
-const request = require('request');
-
-request(myArgs[0], function (error, response, body) {
-  if (!error && response) {
-    console.log('code:', response.statusCode);
-  }
-});
