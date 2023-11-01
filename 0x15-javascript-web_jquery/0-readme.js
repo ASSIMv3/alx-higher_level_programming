30c78c594bdb6b38f1a93d29d35d88bfa378c552
ASSIMv3 <hassanassim01@gmail.com>
2023-11-01 06:06:48 -0700

    mandatory task 0

---
+++ 0x15-javascript-web_jquery/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+const fs = require('fs');
+
+fs.readFile(myArgs[0], 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
