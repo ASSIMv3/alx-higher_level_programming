850ad94f39e437223684e09598f5a528496204bb
ASSIMv3 <hassanassim01@gmail.com>
2023-11-01 06:07:15 -0700

    mandatory task 2

---
+++ 0x15-javascript-web_jquery/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myArgs = process.argv.slice(2);
+const request = require('request');
+
+request(myArgs[0], function (error, response, body) {
+  if (!error && response) {
+    console.log('code:', response.statusCode);
+  }
+});
